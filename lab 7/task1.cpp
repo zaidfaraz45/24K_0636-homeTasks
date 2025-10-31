c43a1195a59af11f6e970a4a917848cbf32aaae9
Zaid Faraz <zaidfarazsyed@gmail.com>
2025-10-31 22:51:38 +0500

    To be done

--- lab 7/task1.cpp
+++ lab 7/task1.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 using namespace std;
 
+struct StudentRecord
+{
+    string name;
+    int score;
+};
+
+struct Node
+{
+    StudentRecord record;
+    Node *next;
+
+    Node(string name, int score): record{name, score}, next(nullptr) {}
+};
+
+class LinkedList
+{
+    Node *head;
+    Node *tail;
+
+    public:
+        void insertAtHead(string n, int s)
+        {
+            if (head == nullptr)
+            {
+                
+            }
+        }
+};
+
 int main()
 {
     
